3ecf2442a6ea2fa775c807bdf679f821d04fafe5
Mike Walmsley <walmsleymk1@gmail.com>
2018-08-07 16:58:59 +0100

    Missed from previous commit

--- zoobot/tests/read_tfrecord_test.py
+++ zoobot/tests/read_tfrecord_test.py
@@ -3,9 +3,10 @@ import pytest
 from zoobot import read_tfrecord
 
 
-def test_load_examples_from_tfrecord():
+def test_load_examples_from_tfrecord(example_tfrecord):
+    tfrecord_locs = [example_tfrecord['loc']]
     examples = read_tfrecord.load_examples_from_tfrecord([tfrecord_loc], 1, size, channels)
     # TODO asserts
 
 
-def 
+def test_show_example()
