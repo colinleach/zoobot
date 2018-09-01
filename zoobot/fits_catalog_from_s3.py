694ac731cbb6dbd21420d9995b82a9574c330e9f
RustyPanda <walmsleymk1@gmail.com>
2018-09-01 15:43:54 +0100

    Save to root

--- zoobot/fits_catalog_from_s3.py
+++ zoobot/fits_catalog_from_s3.py
@@ -24,6 +24,7 @@ predictions_loc = os.path.join(TEST_EXAMPLE_DIR, 'panoptes_predictions.csv')  # 
 df = pd.read_csv(
     predictions_loc, 
     dtype={'id_loc': str, 'label': float},
+    usecols=['fits_loc', 'fits_loc_relative', 'id_loc', 'label']
     # nrows=10
 )
 
