75ee83fe08eb91fc229a58af82692147d27bfd41
Mike Walmsley <walmsleymk1@gmail.com>
2018-12-11 10:53:38 +0000

    MSE comparison (with dropout)

--- zoobot/estimators/bayesian_estimator_funcs.py
+++ zoobot/estimators/bayesian_estimator_funcs.py
@@ -175,11 +175,14 @@ class BayesianModel():
             # mean_loss = binomial_loss(labels, predictions) + penalty_if_not_probability(predictions) + l2_loss
 
             # cross-entropy loss (assumes noisy labels and that prediction is linear and unscaled i.e. logits)
-            onehot_labels = tf.one_hot(tf.cast(labels, tf.int32), depth=2)
+            # onehot_labels = tf.one_hot(tf.cast(labels, tf.int32), depth=2)
             # print_op = tf.print('onehot_labels', onehot_labels)
             # with tf.control_dependencies([print_op]):
-            loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=onehot_labels, logits=predictions)
-            mean_loss = tf.reduce_mean(loss)
+            # loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=onehot_labels, logits=predictions)
+            # mean_loss = tf.reduce_mean(loss)
+
+            # Calculate loss using mean squared error - untested
+            mean_loss = tf.reduce_mean(tf.abs(predictions - labels))
 
 
             # Calculate loss using mean squared error + L2 - untested
