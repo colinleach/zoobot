33e4d1726fb2ff38b0b299c420907b46a241ff66
Mike Walmsley <walmsleymk1@gmail.com>
2018-12-19 13:30:25 +0000

    Allow warm start to refresh index, if no index

--- zoobot/active_learning/execute.py
+++ zoobot/active_learning/execute.py
@@ -73,7 +73,8 @@ class ActiveConfig():
 
         # state for train records is entirely on disk, to allow for warm starts
         self.train_records_index_loc = os.path.join(self.run_dir, 'requested_tfrecords_index.json')
-        if not self.warm_start or not os.path.isfile(self.train_records_index_loc):
+        if not os.path.isfile(self.train_records_index_loc):
+            logging.info('creating new tfrecord index')
             self.write_train_records_index([self.shards.train_tfrecord_loc])  # will append new shards
 
 
