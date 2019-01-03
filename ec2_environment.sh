1400f146f90c24e8af72ea307ef0c302df1171a7
Mike Walmsley <walmsleymk1@gmail.com>
2019-01-03 17:06:33 +0000

    Reorder env script

--- ec2_environment.sh
+++ ec2_environment.sh
@@ -4,14 +4,14 @@ sudo mkfs -t ext4 /dev/xvdb && \
 mkdir root && \
 sudo mount /dev/xvdb root && \
 sudo chmod 777 root && cd root && \
-source activate tensorflow_p36 && \
 aws s3 cp s3://galaxy-zoo/github ~/.ssh/github  && \
 aws s3 cp s3://galaxy-zoo/github.pub ~/.ssh/github.pub  && \
 eval "$(ssh-agent -s)"  && \
 chmod 400 ~/.ssh/github  && \
 ssh-add ~/.ssh/github  && \
 yes | git clone git@github.com:mwalmsley/zoobot.git && \
-cd zoobot && git checkout al-binomial && cd ../ && \
+cd zoobot && git checkout al-iter && cd ../ && \
+source activate tensorflow_p36 && \
 pip install -r zoobot/requirements.txt && \
 pip install -e zoobot  && \
 git clone https://github.com/mwalmsley/shared-astro-utilities.git && \
