262708c272ddf1cd4cf3106d9232db03c499d9f0
Mike Walmsley <walmsleymk1@gmail.com>
2019-01-04 12:05:52 +0000

    Baseline also

--- zoobot/active_learning/analysis.py
+++ zoobot/active_learning/analysis.py
@@ -267,7 +267,7 @@ def identify_catalog_subjects_history(tfrecord_locs, catalog):
 def show_catalog_col_by_iteration(catalog_history, catalog_col, save_loc):
     fig, axes = plt.subplots(nrows=len(catalog_history), sharex=True)
     for iteration_n, df in enumerate(catalog_history):
-        axes[iteration_n].hist(df[catalog_col], density=True)
+        axes[iteration_n].hist(df[catalog_col], density=True, bins=60)
     axes[-1].set_xlabel(catalog_col)
     fig.tight_layout()
     fig.savefig(save_loc)
@@ -305,11 +305,11 @@ if __name__ == '__main__':
     channels = 3
 
     catalog = pd.read_csv(args.catalog_loc)
+    catalog_cols = ['subject_id', 'smooth-or-featured_smooth_fraction']
 
     active_train_locs = get_final_train_locs(args.active_dir)
     show_subjects_by_iteration(active_train_locs, n_subjects, size, channels, os.path.join(args.output_dir, 'subject_history_active.png'))
     active_history = identify_catalog_subjects_history(active_train_locs, catalog)
-    catalog_cols = ['subject_id', 'smooth-or-featured_smooth_fraction']
     for col in catalog_cols:
         show_catalog_col_by_iteration(
             active_history, 
@@ -327,6 +327,14 @@ if __name__ == '__main__':
     if args.baseline_dir is not None:
         baseline_train_locs = get_final_train_locs(args.baseline_dir)
         show_subjects_by_iteration(baseline_train_locs, n_subjects, size, channels, os.path.join(args.output_dir, 'subject_history_baseline.png'))
+        
+        for col in catalog_cols:
+            show_catalog_col_by_iteration(
+                active_history, 
+                col, 
+                os.path.join(args.output_dir, '{}_history_baseline.png'.format(col))
+            )
+
         baseline_log_loc = find_log(args.baseline_dir)
         baseline_save_loc = os.path.join(args.output_dir, 'acc_metrics_baseline_' + name + '.png')
         baseline_smooth_metrics = get_smooth_metrics_from_log(baseline_log_loc, name='baseline')
