fc9809deed6769e47cfc89afc217442b89f63014
Mike Walmsley <walmsleymk1@gmail.com>
2019-01-10 14:39:59 +0000

    Revert to Adam on zoobot

--- run_zoobot_on_panoptes.py
+++ run_zoobot_on_panoptes.py
@@ -113,8 +113,8 @@ if __name__ == '__main__':
     )
 
     model = bayesian_estimator_funcs.BayesianModel(
-        learning_rate=0.001,  # TODO remove this
-        optimizer=tf.train.RMSPropOptimizer,  # TODO insert here directly
+        learning_rate=0.001,
+        optimizer=tf.train.AdamOptimizer
         conv1_filters=32,
         conv1_kernel=3,
         conv2_filters=64,
