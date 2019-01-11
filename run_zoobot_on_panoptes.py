5e5d2c2651e24da6ccc86ac6636c4635c5da3900
Mike Walmsley <walmsleymk1@gmail.com>
2019-01-11 12:34:52 +0000

    Fix learning rate

--- run_zoobot_on_panoptes.py
+++ run_zoobot_on_panoptes.py
@@ -117,7 +117,7 @@ if __name__ == '__main__':
     )
 
     model = bayesian_estimator_funcs.BayesianModel(
-        learning_rate=0.1,
+        learning_rate=0.001,
         optimizer=tf.train.AdamOptimizer,
         conv1_filters=32,
         conv1_kernel=3,
