823414e4b2c883230831d8673eb9329b301c2976
Mike Walmsley <walmsleymk1@gmail.com>
2019-01-15 10:22:38 +0000

    Move shard config

--- zoobot/active_learning/execute.py
+++ zoobot/active_learning/execute.py
@@ -249,6 +249,18 @@ if __name__ == '__main__':
 
     # shards to use
     shard_config = make_shards.load_shard_config(args.shard_config_loc)
+    attrs = [
+        'train_tfrecord_loc',
+        'eval_tfrecord_loc',
+        'labelled_catalog_loc',
+        'unlabelled_catalog_loc',
+        'config_save_loc',
+        'db_loc']
+    for attr in attrs:
+        old_loc = getattr(shard_config, attr)
+        new_loc = os.path.join('data/gz2_shards', os.path.split(old_loc)[-1])
+        print(attr, new_loc)
+        setattr(shard_config, attr, new_loc)
     
     active_config = ActiveConfig(
         shard_config, 
