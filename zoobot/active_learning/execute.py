34c01d3dbea71deb109685e6d23a86f7634df3b5
Mike Walmsley <walmsleymk1@gmail.com>
2019-01-21 15:13:38 +0000

    Change iteration num - is that the prob?

--- zoobot/active_learning/execute.py
+++ zoobot/active_learning/execute.py
@@ -126,12 +126,12 @@ class ActiveConfig():
 
         # iteration_n = 0
         # initial_estimator_ckpt = self.initial_estimator_ckpt  # for first iteration, the first model is the one passed to ActiveConfig
-        # initial_db_loc = self.db_loc
-        # initial_train_tfrecords= [self.shards.train_tfrecord_loc]
+        initial_db_loc = self.db_loc
+        initial_train_tfrecords = [self.shards.train_tfrecord_loc]
 
         iteration_n = 1
-        initial_db_loc = 'data/gz2_shards/runs_cache/iteration_0th_only.db'
-        initial_train_tfrecords = [self.shards.train_tfrecord_loc, 'data/gz2_shards/run_cache/acquired_from_0th_iter.tfrecord']
+        # initial_db_loc = 'data/gz2_shards/runs_cache/iteration_0th_only.db'
+        # initial_train_tfrecords = [self.shards.train_tfrecord_loc, 'data/gz2_shards/run_cache/acquired_from_0th_iter.tfrecord']
 
         epochs = 650
         learning_rate = 0.001
