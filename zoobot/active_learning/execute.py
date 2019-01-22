ac8106e70fe94b137d2ab6c93a1ce0cab0e5af5e
Mike Walmsley <walmsleymk1@gmail.com>
2019-01-22 13:46:14 +0000

    Learn from initial + 30k random

--- zoobot/active_learning/execute.py
+++ zoobot/active_learning/execute.py
@@ -131,7 +131,9 @@ class ActiveConfig():
 
         iteration_n = 1
         initial_db_loc = 'data/gz2_shards/runs_cache/iteration_0th_only.db'
-        initial_train_tfrecords = [self.shards.train_tfrecord_loc, 'data/gz2_shards/runs_cache/acquired_from_0th_iter.tfrecord']
+        # initial_train_tfrecords = [self.shards.train_tfrecord_loc, 'data/gz2_shards/runs_cache/acquired_from_0th_iter.tfrecord']
+        initial_train_tfrecords = [self.shards.train_tfrecord_loc, 'data/gz2_shards/runs_cache/30k_random.tfrecord']
+        
 
         epochs = 650
         learning_rate = 0.001
