fffa6e718841f6bb5027847f00a17f82bf4ffff2
Mike Walmsley <walmsleymk1@gmail.com>
2019-01-28 10:36:13 +0000

    Get 30k random uints

--- zoobot/active_learning/mock_panoptes.py
+++ zoobot/active_learning/mock_panoptes.py
@@ -58,6 +58,6 @@ def get_labels():
 
 if __name__ == '__main__':
     # fill out subjects_requested so that we acquire many new random shards
-    unlabelled_catalog = pd.read_csv('data/gz2_shards/unlabelled_catalog.csv')
+    unlabelled_catalog = pd.read_csv('data/gz2_shards/uint8_256px/unlabelled_catalog.csv')
     subject_ids = list(unlabelled_catalog.sample(30000)['id_str'].astype(str))
     request_labels(subject_ids)  # will write to updated loc
