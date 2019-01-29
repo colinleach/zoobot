f667d68132441f7d9274378424b9cd049298055c
Mike Walmsley <walmsleymk1@gmail.com>
2019-01-29 09:49:17 +0000

    Typo

--- zoobot/active_learning/make_shards.py
+++ zoobot/active_learning/make_shards.py
@@ -233,7 +233,7 @@ if __name__ == '__main__':
     catalog['label'] = catalog['t04_spiral_a08_spiral_count']
 
     catalog['file_loc'] = catalog['png_loc']
-    catalog['file_loc'] = catalog['file_loc'].apply(lambda x: 'data/gz2_shards/' + x.lstrip('/Volumes/alpha')  # active learning will load from png by default
+    catalog['file_loc'] = catalog['file_loc'].apply(lambda x: 'data/gz2_shards/' + x.lstrip('/Volumes/alpha'))  # active learning will load from png by default
     print(catalog['file_loc'].sample())
     print(len(catalog))
     exit(0)
