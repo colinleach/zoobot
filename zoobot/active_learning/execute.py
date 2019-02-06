4d54b7aa117b0b65ac7be938a0c09cede82b90a0
Mike Walmsley <walmsleymk1@gmail.com>
2019-02-06 09:16:30 +0000

    Debug the log

--- zoobot/active_learning/execute.py
+++ zoobot/active_learning/execute.py
@@ -243,7 +243,7 @@ if __name__ == '__main__':
         format='%(asctime)s %(message)s',
         level=logging.DEBUG
     )
-    logging.getLogger().addHandler(logging.StreamHandler())
+    # logging.getLogger().addHandler(logging.StreamHandler())
 
     # instructions for the run
     if args.test:  # do a brief run only
@@ -301,7 +301,7 @@ if __name__ == '__main__':
         n_samples = 15
 
     assert os.path.exists(log_loc)
-    
+
     ###
     iterations_record = active_config.run(train_callable, acquisition_func, n_samples)
     ###
