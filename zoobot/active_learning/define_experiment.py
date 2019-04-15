8e4a426fe48178b33484fb0fb56755e46405d740
Mike Walmsley <walmsleymk1@gmail.com>
2019-04-15 10:33:53 +0100

    Prepare define_experiment to make all weak bar catalogs - command-line args - no filtering to first 20k

--- zoobot/active_learning/define_experiment.py
+++ zoobot/active_learning/define_experiment.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import logging
+import argparse
 
 import pandas as pd
 
@@ -10,7 +11,7 @@ def get_experiment_catalogs(catalog, question, save_dir):
     catalog = shuffle(catalog)  # crucial for GZ2!
     catalog = define_identifiers(catalog)
     labelled, unlabelled = split_labelled_and_unlabelled(catalog, question)
-    labelled = define_labels(labelled, question)
+    labelled = define_labels(labelled, question)  # unlabelled and catalog have no 'label' column
     return catalog, labelled, unlabelled
 
 
@@ -37,26 +38,19 @@ def define_identifiers(catalog):
 
 
 def subject_is_retired(subject):
-    return subject['smooth-or-featured_total-votes'] > 1 # TODO !!temporary until first big reduction! Will be 37ish 
-
-
-def if_not_null_make_int(value):
-    if pd.isnull(value):
-        return value
-    else:
-        return int(value)
+    return subject['smooth-or-featured_total-votes'] > 36
 
 
 def define_labels(catalog, question):
-    catalog['total_votes'] = catalog['smooth-or-featured_total-votes'].apply(if_not_null_make_int)
+    catalog['total_votes'] = catalog['smooth-or-featured_total-votes'].astype(int)
     if question == 'smooth':
-        catalog['label'] = catalog['smooth-or-featured_smooth'].apply(if_not_null_make_int)
+        catalog['label'] = catalog['smooth-or-featured_smooth'].astype(int)
     elif question == 'bar':
-        catalog['total_votes'] = catalog['bar_total-votes'].apply(if_not_null_make_int)
+        catalog['total_votes'] = catalog['bar_total-votes'].astype(int)
         try:
-            catalog['label'] = catalog['bar_weak'].apply(if_not_null_make_int)  # DECALS
+            catalog['label'] = catalog['bar_weak'].astype(int)  # DECALS
         except KeyError:
-            catalog['label'] = catalog['bar_yes'].apply(if_not_null_make_int)  # GZ2
+            catalog['label'] = catalog['bar_yes'].astype(int)  # GZ2
     else:
         raise ValueError('question {} not understood'.format(question))
 
@@ -78,26 +72,36 @@ def get_mock_catalogs(labelled_catalog, save_dir, train_size=256, eval_size=2500
 
 if __name__ == '__main__':
 
-    name = 'smooth_unfiltered'
-    question = 'smooth'
+    # master_catalog_loc = 'data/decals/decals_master_catalog.csv'  # currently with all galaxies but only a few classifications
+    # catalog_dir = 'data/decals/prepared_catalogs/{}'.format(name)
 
-    master_catalog_loc = 'data/decals/decals_master_catalog.csv'  # currently with all galaxies but only a few classifications
-    catalog_dir = 'data/decals/prepared_catalogs/{}'.format(name)
-    if os.path.isdir(catalog_dir):
-        shutil.rmtree(catalog_dir)
-    os.mkdir(catalog_dir)
+    parser = argparse.ArgumentParser(description='Define experiment (labelled catalog, question, etc) from master catalog')
+    parser.add_argument('--master-catalog-loc', dest='master_catalog_loc', type=str,
+                    help='Name of experiment (save to data')
+    parser.add_argument('--question', dest='question', type=str,
+                    help='Question to answer: smooth or bar')
+    parser.add_argument('--save-dir', dest='name', type=str,
+                    help='Save experiment catalogs here')
+    args = parser.parse_args()
+    master_catalog_loc = args.master_catalog_loc
+    question = args.question
+    save_dir = args.save_dir
+
+    if os.path.isdir(save_dir):
+        shutil.rmtree(save_dir)
+    os.mkdir(save_dir)
 
     master_catalog = pd.read_csv(master_catalog_loc)
-    catalog, labelled, unlabelled = get_experiment_catalogs(master_catalog, question, catalog_dir)
+    catalog, labelled, unlabelled = get_experiment_catalogs(master_catalog, question, save_dir)
 
     # ad hoc filtering here
-    catalog = catalog[:20000]
+    # catalog = catalog[:20000]
 
-    labelled.to_csv(os.path.join(catalog_dir, 'labelled_catalog.csv'), index=False)
-    unlabelled.to_csv(os.path.join(catalog_dir, 'unlabelled_catalog.csv'), index=False)
-    catalog.to_csv(os.path.join(catalog_dir, 'full_catalog.csv'), index=False)
+    labelled.to_csv(os.path.join(save_dir, 'labelled_catalog.csv'), index=False)
+    unlabelled.to_csv(os.path.join(save_dir, 'unlabelled_catalog.csv'), index=False)
+    catalog.to_csv(os.path.join(save_dir, 'full_catalog.csv'), index=False)
 
-    simulation_dir = os.path.join(catalog_dir, 'simulation_context')
+    simulation_dir = os.path.join(save_dir, 'simulation_context')
     if not os.path.isdir(simulation_dir):
         os.mkdir(simulation_dir)
     mock_labelled, mock_unlabelled, oracle = get_mock_catalogs(labelled, simulation_dir)
