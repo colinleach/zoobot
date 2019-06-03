1f4f73d128796220188cee2bcd6472f61ba0464a
Mike Walmsley <walmsleymk1@gmail.com>
2019-06-03 16:41:54 +0100

    API works successfully :) but not suitable for what-if :/

--- zoobot/serving/client.py
+++ zoobot/serving/client.py
@@ -1,9 +1,11 @@
 import os
-from Typing import List
+from typing import List
+from tqdm import tqdm
 
 import requests
 import numpy as np
 from PIL import Image
+import json
 
 class Server:
 
@@ -21,31 +23,54 @@ class Server:
     def classify_image(self, image: List):
         url = 'http://{}:{}/v1/models/{}:predict'.format(self.host, self.port, self.model_name)
         assert isinstance(image, list)
-        assert isinstance(image[0][0][0], np.uint8)
-        data =  {
-            "instances": image
-        }
-        response = requests.post(url, data)
+        # assert isinstance(image[0][0][0], int)
+        json_data =  {"instances": [image]}
+        json_str = json.dumps(json_data)
+        # json_str = r'{"instances": 12}'
+        # print(json_str)
+        # print(json.loads(json_str))
+        response = requests.post(url, data=json_str)
         print(response)
-        return response.json
+        return response.json()
+
+    def classify_images(self, paths: List):
+        results = []
+        for path in tqdm(paths):
+            image = load_image(path)
+            results.append([self.classify_image(image)])
+        return results
 
 
 def load_image(path: str):
     im = Image.open(path)
     reshaped = im.resize((256, 256))  # must match serving reciever fn config
-    matrix = np.array(reshaped).astype(np.uint8)
+    matrix = np.array(reshaped).astype(float) # .astype(np.uint8)
     assert matrix.shape == (256, 256, 3)
-    return matrix
+    return matrix.tolist()
 
 if __name__ == '__main__':
 
     host = '3.82.176.136'
     port = '8501'
-    model_name = 'TODO'
-    test_image_loc = 'zoobot/tests/test_examples/example_a.png'  # relative path, careful
-    assert os.path.exists(test_image_loc)
+    model_name = '1559567967'
+    hard_image_loc = 'zoobot/tests/test_examples/example_a.png'  # relative path, careful
+    assert os.path.exists(hard_image_loc)
+    smooth_image_loc = 'zoobot/tests/test_examples/smooth.png'  # relative path, careful
+    assert os.path.exists(smooth_image_loc)
+    featured_image_loc = 'zoobot/tests/test_examples/featured.png'
+
+    # image = load_image(featured_image_loc)
+    # with open('temp.txt', 'w') as f:
+    #     f.write(str(image))
+    # exit(0)
 
     server = Server(host, port, model_name)
-    server.check_up()
-    image = load_image(test_image_loc)
-    server.classify_image(image)
+    status = server.check_up()
+    print(status)
+
+    # image = load_image(hard_image_loc)
+    # print(server.classify_image(image))
+
+
+    results = server.classify_images([hard_image_loc, smooth_image_loc, featured_image_loc])
+    print(results)
