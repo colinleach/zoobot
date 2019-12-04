94824e54881b51258ecc87787381b6bd2efe75fa
Mike Walmsley <walmsleymk1@gmail.com>
2019-12-04 16:56:40 +0000

    nothing

--- configure_panoptes.py
+++ configure_panoptes.py
@@ -25,7 +25,7 @@ def configure_designator(subject_set_chances, workflow_id, login_loc, debug=Fals
 
 
 def upload_dummy_subjects(catalog_loc, project_id, workflow_id, login_loc):
-    # only for testing 
+    # only for testing
 
     catalog = pd.read_csv(catalog_loc, nrows=10000)
 
@@ -34,8 +34,10 @@ def upload_dummy_subjects(catalog_loc, project_id, workflow_id, login_loc):
     catalog.to_csv(dummy_catalog_loc, index=False)
 
     featured_col = 'smooth-or-featured_featured-or-disk'
-    most_featured = catalog.sort_values(featured_col, ascending=False)[:80]['id_str'].values
-    least_featured = catalog.sort_values(featured_col, ascending=True)[:500]['id_str'].values
+    most_featured = catalog.sort_values(featured_col, ascending=False)[
+        :80]['id_str'].values
+    least_featured = catalog.sort_values(featured_col, ascending=True)[
+        :500]['id_str'].values
 
     panoptes = mock_panoptes.Panoptes(
         catalog_loc=dummy_catalog_loc,
@@ -53,17 +55,20 @@ def upload_dummy_subjects(catalog_loc, project_id, workflow_id, login_loc):
 
 if __name__ == '__main__':
 
-    project_id = '8751'  # DUMMY PROJECT
-    workflow_id = '10582' # REAL enhanced workflow
+    project_id = '8751'  #  DUMMY PROJECT
+    workflow_id = '10582'  # REAL enhanced workflow
     login_loc = 'zooniverse_login.json'
-    catalog_loc = 'data/decals/decals_master_catalog.csv'
 
+    # catalog_loc = 'data/decals/decals_master_catalog.csv'
     # upload_dummy_subjects(catalog_loc, project_id, workflow_id, login_loc)
 
     subject_set_chances = {
-            '74909': 0.8,  # priority
-            '74905': 0.2  # random
+        '74909': 0.8,  # priority
+        '74905': 0.2  # random
     }
-    configure_designator(subject_set_chances, workflow_id, login_loc, debug=True)
-
-
+    configure_designator(
+        subject_set_chances,
+        workflow_id,
+        login_loc,
+        debug=True
+    )
