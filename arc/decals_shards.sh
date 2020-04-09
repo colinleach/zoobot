9e35bf0b25b7c30943645982fae9d4baf4d86303
Mike Walmsley <walsleymk1@gmail.com>
2020-04-09 13:04:59 +0100

    Typo

--- arc/decals_shards.sh
+++ arc/decals_shards.sh
@@ -7,4 +7,4 @@
 
 module purge
 
-python /data/phys-zooniverse/chri5177/repos/zoobot/make_decals_tfrecords.py --labelled-catalog=/home/walml/repos/zoobot/data/latest_labelled_catalog_256.csv --eval-size=5525 --shard-dir=/data/phys-zooniverse/chri5177/repos/zoobot/data/decals/shards/multilabel_256 --img-size 256
+python /data/phys-zooniverse/chri5177/repos/zoobot/make_decals_tfrecords.py --labelled-catalog=/data/phys-zooniverse/chri5177/repos/zoobot/data/latest_labelled_catalog_256.csv --eval-size=5525 --shard-dir=/data/phys-zooniverse/chri5177/repos/zoobot/data/decals/shards/multilabel_256 --img-size 256
