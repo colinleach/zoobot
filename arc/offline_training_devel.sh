6c524f38ec60b38634d48c43154eab6ded7bf0ec
Mike Walmsley <walsleymk1@gmail.com>
2020-04-13 12:11:21 +0100

    Split main and devel

--- arc/offline_training_devel.sh
+++ arc/offline_training_devel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --partition=htc
+#SBATCH --partition=htc-devel
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=02:00:00
-#SBATCH --gres=gpu:p100:2
+#SBATCH --time=00:10:00
+#SBATCH --gres=gpu:1
 #SBATCH --mem=12288
-#SBATCH --job-name=offline_training
+#SBATCH --job-name=offline_training_devel
 
 module purge
 module load python/anaconda3/2019.03
