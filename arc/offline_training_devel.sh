7265c5ea64f3fe78f2174ffe8b6ed571b7e76f3b
Mike Walmsley <walsleymk1@gmail.com>
2020-04-15 12:01:26 +0100

    Run both minimal (with profiling) and full

--- arc/offline_training_devel.sh
+++ arc/offline_training_devel.sh
@@ -13,4 +13,9 @@ module load gpu/cuda/10.1.243
 module load gpu/cudnn/7.5.0__cuda-10.0
 
 # source activate $DATA/envs/zoobot
+
+# minimal example, works but profiler cannot load without CUPTI
 $DATA/envs/zoobot/bin/python $DATA/repos/zoobot/minimal_tensorflow.py
+
+# full code, segfaults when calculations begin
+$DATA/envs/zoobot/bin/python offline_training.py --experiment-dir /data/phys-zooniverse/chri5177/repos/zoobot/results/latest_offline --shard-img-size 256 --train-dir /data/phys-zooniverse/chri5177/repos/zoobot/data/decals/shards/multilabel_256/train --eval-dir /data/phys-zooniverse/chri5177/repos/zoobot/data/decals/shards/multilabel_256/eval --epochs 150 
