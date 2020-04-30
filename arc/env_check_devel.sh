76f2a21e310890ff8fee400e44b5626911b33196
Mike Walmsley <walsleymk1@gmail.com>
2020-04-30 13:36:06 +0100

    Add comments

--- arc/env_check_devel.sh
+++ arc/env_check_devel.sh
@@ -14,8 +14,10 @@ module load gpu/cudnn/7.5.0__cuda-10.0
 
 # minimal Tensorflow example:
 
+echo 'Running TF 2.1'
 # with my env (TF 2.1), cannot load CUDA without cudnnn >7.6.4
-# $DATA/envs/zoobot/bin/python $DATA/repos/zoobot/minimal_tensorflow.py
+$DATA/envs/zoobot/bin/python $DATA/repos/zoobot/minimal_tensorflow.py
 
+echo 'Running alternative env'
 # Yassamine's env only has TF 1.10
 $DATA/tensor-env/bin/python $DATA/repos/zoobot/minimal_tensorflow.py
