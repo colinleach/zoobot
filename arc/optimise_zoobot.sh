f55d8f3ab36fc10b48ec6813da5977488caa47d2
Mike Walmsley <walsleymk1@gmail.com>
2020-05-04 10:38:51 +0100

    More debugging

--- arc/optimise_zoobot.sh
+++ arc/optimise_zoobot.sh
@@ -9,8 +9,12 @@
 
 module purge
 module load python/anaconda3/2019.03
-# module load gpu/cuda/10.1.243
+module load gpu/cuda/10.1.243
 module load gpu/cudnn/7.6.5__cuda-10.1
+
+ls /system/software/arcus-htc/gpu/cudnn/7.6.5/cuda10.1/lib64
+
+nvidia-smi
 
 # minimal Tensorflow example:
 $DATA/envs/zoobot/bin/python $DATA/repos/zoobot/minimal_tensorflow.py
