950c20331d131ef30d276adb46c6a9a0028e6532
Mike Walmsley <walsleymk1@gmail.com>
2020-05-04 11:35:38 +0100

    run actual optimiser

--- arc/env_check_devel.sh
+++ arc/env_check_devel.sh
@@ -12,6 +12,8 @@ module load python/anaconda3/2019.03
 module load gpu/cuda/10.1.243
 module load gpu/cudnn/7.6.5__cuda-10.1
 
+echo $LD_LIBRARY_PATH
+
 ls /system/software/arcus-htc/cuda-toolkit/10.1.243/lib64
 ls /system/software/arcus-htc/cuda-toolkit/10.1.243/lib
 ls /system/software/arcus-htc/gpu/cudnn/7.6.5/cuda10.1/lib64
--- arc/optimise_zoobot.sh
+++ arc/optimise_zoobot.sh
@@ -12,13 +12,9 @@ module load python/anaconda3/2019.03
 module load gpu/cuda/10.1.243
 module load gpu/cudnn/7.6.5__cuda-10.1
 
-ls /system/software/arcus-htc/gpu/cudnn/7.6.5/cuda10.1/lib64
-
-nvidia-smi
-
 # minimal Tensorflow example:
-$DATA/envs/zoobot/bin/python $DATA/repos/zoobot/minimal_tensorflow.py
+# $DATA/envs/zoobot/bin/python $DATA/repos/zoobot/minimal_tensorflow.py
 
 # research code:
-# $DATA/envs/zoobot/bin/python $DATA/repos/zoobot/optimise_zoobot.py
+$DATA/envs/zoobot/bin/python $DATA/repos/zoobot/optimise_zoobot.py
 
