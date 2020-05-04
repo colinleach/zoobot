aa4059f5364ce8c01031b59fb9588379089b1580
Mike Walmsley <walsleymk1@gmail.com>
2020-05-04 10:35:57 +0100

    Try only cudnn

--- arc/optimise_zoobot.sh
+++ arc/optimise_zoobot.sh
@@ -9,7 +9,7 @@
 
 module purge
 module load python/anaconda3/2019.03
-module load gpu/cuda/10.1.243
+# module load gpu/cuda/10.1.243
 module load gpu/cudnn/7.6.5__cuda-10.1
 
 # minimal Tensorflow example:
