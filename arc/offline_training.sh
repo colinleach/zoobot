6002707f122371be9e99b69ed16d84e7dcc2e0bc
Mike Walmsley <walsleymk1@gmail.com>
2020-05-05 18:39:10 +0100

    Try remove gpu constraint in prod. version

--- arc/offline_training.sh
+++ arc/offline_training.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=htc
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=02:00:00
-#SBATCH --gres=gpu:v100:1
-#SBATCH --job-name=offline_training_v100x1
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=offline_training_x1
 
 module purge
 module load python/anaconda3/2019.03
@@ -12,9 +12,9 @@ module load gpu/cuda/10.1.243
 module load gpu/cudnn/7.6.5__cuda-10.1
 
 epochs=1000
-batch_size=64
+batch_size=128
 shard_img_size=256
-final_size=128
+final_size=224
 shard_dir=$DATA/repos/zoobot/data/decals/shards/multilabel_master_filtered_$shard_img_size
 
 $DATA/envs/zoobot/bin/python offline_training.py --experiment-dir $DATA/repos/zoobot/results/latest_offline --shard-img-size $shard_img_size --train-dir $shard_dir/train --eval-dir $shard_dir/eval --epochs $epochs --batch-size $batch_size --final-size $final_size  
