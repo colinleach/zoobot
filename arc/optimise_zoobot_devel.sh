7c0cdf141d92201287d50fd02db0eab83bd75ce4
Mike Walmsley <walsleymk1@gmail.com>
2020-05-05 11:08:42 +0100

    More debugs

--- arc/optimise_zoobot_devel.sh
+++ arc/optimise_zoobot_devel.sh
@@ -12,6 +12,10 @@ module load python/anaconda3/2019.03
 module load gpu/cuda/10.1.243
 module load gpu/cudnn/7.6.5__cuda-10.1
 
+echo $LD_LIBRARY_PATH
+
+ls /system/software/arcus-htc/gpu/cudnn/7.6.5/cuda10.1/lib64
+
 nvidia-smi
 
 # minimal Tensorflow example:
