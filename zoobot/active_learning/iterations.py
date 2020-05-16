747d1ae698117fe932e47b54421bd29192d2c37b
Mike Walmsley <walsleymk1@gmail.com>
2020-05-16 13:39:09 +0100

    Typo

--- zoobot/active_learning/iterations.py
+++ zoobot/active_learning/iterations.py
@@ -221,7 +221,7 @@ class Iteration():
         logging.info('Saving to {}'.format(self.estimators_dir))
 
         # TEMP for debugging acquisitions
-        skip_model_dir = os.path.dirname(self.prediction_shards) + '_final_' + self.iteration_dir[-1]  # e.g. _0, _1
+        skip_model_dir = os.path.dirname(self.prediction_shards[0]) + '_final_' + self.iteration_dir[-1]  # e.g. _0, _1
         if os.path.isdir(skip_model_dir):
             logging.warning('Skipping training and loading cheat estimator at {}'.format(skip_model_dir))
             save_dir = self.estimators_dir + '/models/final'
