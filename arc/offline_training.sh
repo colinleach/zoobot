5e203f0bbc77da9b649e9775bb5865d9d51a9180
Mike Walmsley <walsleymk1@gmail.com>
2020-05-25 13:19:34 +0100

    Run offline training on all of GZ2, 424->224, B0

--- arc/offline_training.sh
+++ arc/offline_training.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --partition=htc
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=08:00:00
+#SBATCH --time=23:55:00
 #SBATCH --gres=gpu:v100:1
 #SBATCH --job-name=offline_training_x1
 
@@ -13,10 +13,12 @@ module load gpu/cudnn/7.6.5__cuda-10.1
 
 epochs=1000
 batch_size=128  # fits on V100, not my laptop...
-shard_img_size=256
+shard_img_size=424
 final_size=224
+shard_dir=$DATA/repos/zoobot/data/gz2/shards/all_featp5_facep5_424_arc
 # shard_dir=$DATA/repos/zoobot/data/decals/shards/multilabel_master_filtered_$shard_img_size
-shard_dir=$DATA/repos/zoobot/data/gz2/shards/multilabel_master_filtered_$shard_img_size
+# shard_dir=$DATA/repos/zoobot/data/gz2/shards/multilabel_master_filtered_$shard_img_size
+
 
 echo $epochs $batch_size $shard_img_size $final_size $shard_dir
 
