82d3ccc9378f6db9ac5a50bd4d9e2cd4b84d6176
Mike Walmsley <chri5177@arcus-htc-login02.arcus-htc.arc.local>
2020-06-11 10:57:06 +0100

    update make_shards with remote changes

--- arc/make_shards.sh
+++ arc/make_shards.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --partition=htc
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=23:50:00
+#SBATCH --time=95:50:00
 #SBATCH --job-name=make_shards
 
 module purge
@@ -10,4 +10,4 @@ module load python/anaconda3/2019.03
 
 export PYTHON=$DATA/envs/zoobot/bin/python
 
-$PYTHON zoobot/active_learning/make_shards.py --labelled-catalog=data/gz2/prepared_catalogs/all_featp5_facep5_arc_unfiltered/simulation_context/labelled_catalog.csv --unlabelled-catalog=data/gz2/prepared_catalogs/all_featp5_facep5_arc_unfiltered/simulation_context/unlabelled_catalog.csv --eval-size 15000 --shard-dir=data/gz2/shards/all_featp5_facep5_sim_300_arc_unfiltered --img-size 300
+$PYTHON zoobot/active_learning/make_shards.py --labelled-catalog=data/gz2/prepared_catalogs/all_featp5_facep5_arc_unfiltered/labelled_catalog.csv --unlabelled-catalog=data/gz2/prepared_catalogs/all_featp5_facep5_arc_unfiltered/unlabelled_catalog.csv --eval-size 15000 --shard-dir=data/gz2/shards/all_featp5_facep5_all_300_arc_unfiltered --img-size 300
