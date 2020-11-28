be861f650793d436ee66b05db06d694a90d7c489
Mike Walmsley <walsleymk1@gmail.com>
2020-11-28 12:19:22 +0000

    rename before running

--- arc/make_shards.sh
+++ arc/make_shards.sh
@@ -12,5 +12,8 @@ export PYTHON=$DATA/envs/zoobot/bin/python
 
 CATALOG_NAME=decals_dr  # min 3 classifications
 
-$PYTHON zoobot/active_learning/make_shards.py --labelled-catalog=data/decals/prepared_catalogs/$CATALOG_NAME/labelled_catalog.csv --unlabelled-catalog=data/decals/prepared_catalogs/$CATALOG_NAME/unlabelled_catalog.csv --eval-size 49700 --shard-dir=data/decals/shards/${CATALOG_NAME}_full  --img-size 300
+# 80% split w/ decals
+$PYTHON zoobot/active_learning/make_shards.py --labelled-catalog=data/decals/prepared_catalogs/$CATALOG_NAME/labelled_catalog.csv --unlabelled-catalog=data/decals/prepared_catalogs/$CATALOG_NAME/unlabelled_catalog.csv --eval-size 49700 --shard-dir=data/decals/shards/${CATALOG_NAME}_train_labelled  --img-size 300
+
+# testing
 # $PYTHON zoobot/active_learning/make_shards.py --labelled-catalog=data/decals/prepared_catalogs/$CATALOG_NAME/labelled_catalog.csv --unlabelled-catalog=data/decals/prepared_catalogs/$CATALOG_NAME/unlabelled_catalog.csv --eval-size 100 --shard-dir=data/decals/shards/$CATALOG_NAME  --img-size 300 --max-labelled 4000
