d07b020a47f99e4f5d80fbde0f08cef788444d34
Mike Walmsley <walsleymk1@gmail.com>
2020-12-18 17:39:40 +0000

    DR2 M1

--- predictions_on_images.py
+++ predictions_on_images.py
@@ -86,13 +86,13 @@ if __name__ == '__main__':
         data_dir = os.environ['DATA']
         logging.info(data_dir)
         # catalog_loc = f'{data_dir}/repos/zoobot/data/decals/decals_master_catalog_arc.csv'
-        model_name = 'decals_dr_train_labelled_m4'
+        model_name = 'decals_dr_train_labelled_m1'
         checkpoint_dir = f'{data_dir}/repos/zoobot/results/{model_name}/in_progress'
         # folder_to_predict = f'{data_dir}/png_native/dr5/J000'
-        folder_to_predict = '/data/phys-zooniverse/chri5177/galaxy_zoo/decals/dr1_dr2/png/dr1/standard'
+        folder_to_predict = '/data/phys-zooniverse/chri5177/galaxy_zoo/decals/dr1_dr2/png/dr2/standard'
         # folder_to_predict = f'{data_dir}/repos/zoobot/data/decals/temp/J000'
         file_format = 'jpeg'
-        folder_name = 'dr1'
+        folder_name = 'dr2'
         save_loc = f'{data_dir}/repos/zoobot/results/folder_{folder_name}_model_{model_name}_predictions.csv'
 
     # go
